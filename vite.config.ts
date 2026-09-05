63c7fff3602b43a7e3d3ffc81ac54eb860e3212b
Siddhesh Pardeshi <siddheshpardeshi2@gmail.com>
2026-09-05 18:15:12 +0530

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/rexus-client/',
+  build: {
+    outDir: 'build',
+  },
 })
